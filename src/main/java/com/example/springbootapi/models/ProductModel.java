f23b04c3447ca6caa840c1001469e748b9498d64
Gabriel Martins <97320295+Dev-Gabriel-Martins@users.noreply.github.com>
2023-08-06 07:41:38 -0300

    init

---
+++ src/main/java/com/example/springbootapi/models/ProductModel.java
@@ -0,0 +1,14 @@
+package com.example.springbootapi.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+public class ProductModel implements Serializable {
+
+
+
+}
